162c82f3321e273710edc966f9df4a8ea875c439
Ciroandrea Gagliano <ciroandrea.gagliano@tc-group.it>
2024-01-29 12:51:51 +0100

    Aggiunte configurazioni di jackson e mvc per formattazioni varie in application.properties

---
+++ CH02-Jackson-Mvc/src/test/java/it/large/library/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package it.large.library.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
