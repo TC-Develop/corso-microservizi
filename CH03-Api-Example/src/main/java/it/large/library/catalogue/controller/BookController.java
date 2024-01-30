e6763fe0bb9128dc2ea33f7eae070d4279feadf2
Ciroandrea Gagliano <ciroandrea.gagliano@tc-group.it>
2024-01-30 12:37:11 +0100

    Modifica capitolo 3

--- CH03-Api-Example/src/main/java/it/large/library/catalogue/controller/BookController.java
+++ CH03-Api-Example/src/main/java/it/large/library/catalogue/controller/BookController.java
@@ -1,10 +1,7 @@
 package it.large.library.catalogue.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // Indica che questa classe è un controller Spring e che ogni metodo gestisce automaticamente la conversione dell'output in formato JSON.
 @RequestMapping("catalogue/api/v1/book") // Specifica il percorso di base per tutte le richieste gestite da questo controller.
@@ -12,13 +9,13 @@ public class BookController {
 
 
     // Indica che questo metodo risponde a richieste HTTP di tipo POST al percorso base specificato per il controller
-    @PostMapping(path = "")
-    public ResponseEntity<String> add(
+    @GetMapping(path = "/{title_book}")
+    public ResponseEntity<String> getByName(
             // Specifica il nome del parametro nella stringa di query dell'URL. In questo caso, si aspetta un parametro con il nome "title_book".
-            @RequestParam(name = "title_book") String titleBook
+            @PathVariable(name = "title_book") String titleBook
     ) {
 
-        String bookTitle = "Questo è il titolo del nuovo libro inserito: " + titleBook;
+        String bookTitle = "Questo è il titolo del libro richiesto: " + titleBook;
 
         // Restituisce una risposta HTTP con status 200 (OK) e il corpo della risposta contiene l'oggetto bookResponse.
         // La classe ResponseEntity consente di personalizzare la risposta HTTP, inclusi lo status e gli header.
