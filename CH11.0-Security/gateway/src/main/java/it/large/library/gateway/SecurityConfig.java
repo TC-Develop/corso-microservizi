eb5e60a6b2a1dc20a7733b045fef4987835aff0a
Francesco Vergara <francesco.vergara@tc-group.it>
2024-03-04 10:28:39 +0100

    Security

--- CH11.0-Security/gateway/src/main/java/it/large/library/gateway/SecurityConfig.java
+++ CH11.0-Security/gateway/src/main/java/it/large/library/gateway/SecurityConfig.java
@@ -1,18 +1,40 @@
 package it.large.library.gateway;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.large.library.gateway.security.filter.JwtLoginFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtLoginFilter jwtLoginFilter(@Autowired AuthenticationManager authenticationManager,
+                                         @Autowired ObjectMapper objectMapper){
+        return new JwtLoginFilter(authenticationManager, objectMapper);
+    }
 
     public List<UserDetails> userList =
             List.of(
@@ -21,11 +43,12 @@ public class SecurityConfig {
             );
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, @Autowired JwtLoginFilter jwtLoginFilter) throws Exception {
         http.authorizeHttpRequests(authorize -> {
             authorize.requestMatchers("/version").permitAll()
                     .requestMatchers("/**").authenticated();
-        });
+        })
+                .addFilterBefore(jwtLoginFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
@@ -34,5 +57,10 @@ public class SecurityConfig {
         return  new InMemoryUserDetailsManager(userList);
     }
 
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
